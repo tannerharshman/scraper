7b62a5df522d00fe416b3510bf2a2d850c4c5745
Tanner H <tannerharshman92@gmail.com>
2023-08-18 15:31:49 -0400

    finished python scraper.

--- python_scraper.py
+++ python_scraper.py
@@ -1,13 +1,25 @@
 import urllib.request
 import re
+import os
 import requests
 from bs4 import BeautifulSoup
 
 
+def dl_image(url, folder_path):
+    response = requests.get(url)
+    if response.status_code == 200:
+        filename = os.path.basename(url)
+        file_path = os.path.join(folder_path, filename)
+        with open(file_path, "wb") as file:
+            file.write(response.content)
+        print(f"Downloaded and saved: {file_path}")
+    else:
+        print(f"Failed to download: {url}")
 
-url = "http://theoatmeal.com"
-response = urllib.request.urlopen(url)
-content = response.read()
+
+a_url = "http://theoatmeal.com"
+a_response = urllib.request.urlopen(a_url)
+content = a_response.read()
 soup = BeautifulSoup(content, "html.parser")
 
 for link in soup.findAll("a"):
@@ -16,6 +28,13 @@ for link in soup.findAll("a"):
         print(my_url)
 
 images = soup.findAll("img")
+download_folder = "oatmeal_pics"
+
+if not os.path.exists(download_folder):
+    os.mkdir(download_folder)
+
 for image in images:
     my_image = image.get('src')
-    print(f"{url}{my_image}")
+    if my_image and re.search(r"^http", my_image):
+        print(f"{my_image}")  # Print the image URL
+        dl_image(my_image, download_folder)
